d423afb946667cf0d3720517b6fe1b747e9ceeb0
luisda-art <luisdavid.rdelahoz@gmail.com>
2025-09-26 17:50:15 -0500

    Update ConsoleNotificationAdapter.java

--- src/com/inventory/patterns/ConsoleNotificationAdapter.java
+++ src/com/inventory/patterns/ConsoleNotificationAdapter.java
@@ -1,21 +1,18 @@
 package com.inventory.patterns;
 
-// Suponiendo que esta es la interfaz que debe implementarse
-public interface NotificationAdapter {
-    void sendNotification(String message);  // Método de la interfaz
-}
 
+public interface NotificationAdapter {
+    void sendNotification(String message);  
+}
 public class ConsoleNotificationAdapter implements NotificationAdapter {
     @Override
     public void sendNotification(String message) {
         System.out.println("\nNOTIFICACIÓN: " + message);
     }
-
     public static void main(String[] args) {
-        // Crear una instancia de ConsoleNotificationAdapter
+      
         NotificationAdapter notification = new ConsoleNotificationAdapter();
 
-        // Enviar una notificación
         notification.sendNotification("Este es un mensaje de prueba.");
     }
 }
